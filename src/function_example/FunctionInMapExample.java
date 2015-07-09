c26a8df583f9968ef1b6974030e77c111edf456f
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-07-09 16:46:27 +0100

    Add Function in map example

---
+++ src/function_example/FunctionInMapExample.java
@@ -0,0 +1,25 @@
+package function_example;
+
+import java.util.List;
+import java.util.function.Function;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+
+public class FunctionInMapExample {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        Function<Integer, Integer> square = number -> number * number;
+
+        List<Integer> numbersSquared = numbers.stream()
+                                              .map(square)
+                                              .collect(toList());
+
+        System.out.println("Original list: " + numbers + "\n" +
+                           "Numbers squared: " + numbersSquared);
+    }
+
+}
