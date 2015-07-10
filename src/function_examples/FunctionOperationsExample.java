29c2f648e23241b8f15d25d7f29bf6c07437c6d3
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-07-10 18:12:15 +0100

    Add Function operations example

---
+++ src/function_examples/FunctionOperationsExample.java
@@ -0,0 +1,18 @@
+package function_examples;
+
+import java.util.function.Function;
+
+public class FunctionOperationsExample {
+
+    public static void main(String[] args) {
+
+        Function<Integer, Integer> plusTwo = number -> number + 2;
+        Function<Integer, Integer> doubleIt = number -> number * 2;
+
+        int result = plusTwo.andThen(doubleIt).apply(3);
+
+        System.out.println("The double of three is: " + doubleIt.apply(3) + "\n" +
+                           "The result of plus two to three and then doubled it is: " + result);
+    }
+
+}
