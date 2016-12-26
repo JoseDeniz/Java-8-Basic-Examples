622079dac90ba68cc474651424d1e51024003115
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2016-12-26 14:57:32 +0000

    Update PatternMatching example

--- src/main/java/javaslang/pattern_matching/FizzBuzzPatternMatching.java
+++ src/main/java/javaslang/pattern_matching/FizzBuzzPatternMatching.java
@@ -6,24 +6,24 @@ import java.util.function.Predicate;
 
 import static javaslang.API.*;
 
-public class PatternMatching {
+public class FizzBuzzPatternMatching {
 
     public static void main(String[] args) {
         String fizzBuzz = List.rangeClosed(1, 100)
-                .map(PatternMatching::parseNumber)
+                .map(FizzBuzzPatternMatching::parseNumber)
                 .reduce((acc, nextValue) -> acc += ", " + nextValue);
 
         System.out.println(fizzBuzz);
     }
 
     private static String parseNumber(Integer number) {
-        final Predicate<Integer> isMultipleOfThree = n -> n % 3 == 0;
-        final Predicate<Integer> isMultipleOfFive = n -> n % 5 == 0;
-        final Predicate<Integer> isMultipleOfFifteen = isMultipleOfThree.and(isMultipleOfFive);
+        final Predicate<Integer> isDivisibleByThree = n -> n % 3 == 0;
+        final Predicate<Integer> isDivisibleByFive = n -> n % 5 == 0;
+        final Predicate<Integer> isDivisibleByThreeAndFive = isDivisibleByThree.and(isDivisibleByFive);
         return Match(number).of(
-                Case(isMultipleOfFifteen, "FizzBuzz"),
-                Case(isMultipleOfThree, "Fizz"),
-                Case(isMultipleOfFive, "Buzz"),
+                Case(isDivisibleByThreeAndFive, "FizzBuzz"),
+                Case(isDivisibleByThree, "Fizz"),
+                Case(isDivisibleByFive, "Buzz"),
                 Case($(), String.valueOf(number))
         );
     }
